5a9e1d62e07ddafa58a9546b3926bd2cb9e67564
Tolga Cesur <ttolgacesur@gmail.com>
2020-01-24 01:23:21 +0300

    Change metric model schema and Remove versionKey

--- src/models/metric.model.ts
+++ src/models/metric.model.ts
@@ -2,16 +2,19 @@ import mongoose from "mongoose";
 
 const MetricSchema = new mongoose.Schema({
     token: String,
-    ttfb: Number,
-    fcp: Number,
-    domComplete: Number,
-    windowLoadEvent: Number,
-    resources: [{
-        url: String,
-        type : String,
-        duration: String,
-        transferSize : String
-    }]
-}, { timestamps: { createdAt: 'createdAt' }});
+    url: String,
+    data : {
+        ttfb: Number,
+        fcp: Number,
+        domComplete: Number,
+        windowLoadEvent: Number,
+        resources: [{
+            url: String,
+            type : String,
+            duration: String,
+            transferSize : String
+        }]
+    }
+}, { timestamps: { createdAt: 'createdAt'}, versionKey: false});
 
 export default mongoose.model("Metric", MetricSchema);
