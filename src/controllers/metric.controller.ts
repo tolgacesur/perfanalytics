47d75954998f1bdbff52fa14d94696dc71396406
Tolga Cesur <ttolgacesur@gmail.com>
2020-01-24 01:22:39 +0300

    Some Fixes for endpoints

--- src/controllers/metric.controller.ts
+++ src/controllers/metric.controller.ts
@@ -4,27 +4,43 @@ import Metric from '../models/metric.model';
 export default class MetricController {
 
     public async saveMetric(req: Request, res: Response) {
-        const {token, metrics} = req.body;
+        const {token, metrics, url} = req.body;
 
+        // TODO : Check if token exists
         if (!token || !metrics){
-            return res.status(422).json(null);
+            return res.status(422).json();
         }
 
-        metrics.token = token;
+        const data = {
+            token,
+            url,
+            data : metrics
+        };
 
-        await new Metric(metrics).save();
+        try {
+            await new Metric(data).save();
+        } catch (error) {
+            console.log(error);
+        }
 
-        return res.send(null);
+        return res.send();
     }
 
     public async getMetrics(req: Request, res: Response) {
         const {token} = req.params;
 
+        // TODO : Check if token exists
         if (!token){
-            return res.status(422).json(null);
+            return res.status(422).json();
         }
 
-        const allMetrics = await Metric.find({token});
+        let allMetrics = [];
+
+        try {
+            allMetrics = await Metric.find({token});
+        } catch (error) {
+            console.log(error);
+        }
 
         return res.json(allMetrics);
     }
