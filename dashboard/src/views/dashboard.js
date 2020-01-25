ec2c69945a036b9ac134625cb643b1afb8d5eb7a
Tolga Cesur <ttolgacesur@gmail.com>
2020-01-26 00:14:30 +0300

    Add chart.js and Fix layout

--- dashboard/src/views/dashboard.js
+++ dashboard/src/views/dashboard.js
@@ -1,23 +1,31 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Layout } from 'antd';
 
 import './dashboard.css';
 import Header from '../components/header/header';
 import Metrics from './metrics/metrics';
 
+const { Content, Footer } = Layout;
+
 const Dashboard = () => {
     return ( 
         <div>
-            <Header></Header>
-            <Router>
-                <Switch>
-                    <Route exac path="/">
-                        <div className="container">
-                            <Metrics/>
-                        </div>
-                    </Route>
-                </Switch>
-            </Router>
+            <Layout className="layout">
+                <Header/>
+                <Content style={{ padding: '0 50px' }}>
+                    <Router>
+                        <Switch>
+                            <Route exac path="/">
+                                <div className="container">
+                                    <Metrics/>
+                                </div>
+                            </Route>
+                        </Switch>
+                    </Router>
+                </Content>
+                <Footer className="footer">Perfanalytics © 2020</Footer>
+            </Layout>
         </div>
     );
 }
--- dashboard/src/views/metrics/metrics.js
+++ dashboard/src/views/metrics/metrics.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Row, Col } from 'antd';
 
 import './metrics.css';
+import Chart from '../../components/chart/chart';
 
 const Metrics = () => {
-    return ( <h1>Charts</h1> );
+    return (
+        <div>
+            <Row>
+                <Col sm={12} md={6}>
+                    <Chart data={[]} title="test" color="#3E517A"/>
+                </Col>
+                <Col sm={12} md={6}>
+                    <Chart data={[]} title="test" color="#3E517A"/>
+                </Col>
+            </Row>
+            <Row>
+                <Col sm={12} md={6}>
+                    <Chart data={[]} title="test" color="#3E517A"/>
+                </Col>
+                <Col sm={12} md={6}>
+                    <Chart data={[]} title="test" color="#3E517A"/>
+                </Col>
+            </Row>
+        </div>
+    );
 }
 
 export default Metrics;
